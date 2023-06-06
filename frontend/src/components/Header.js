ed6494e20c1d37118da28c2d93aace6ddb8280a4
femmor <fegomson@gmail.com>
2023-06-06 12:14:10 -0500

    Display item count in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,11 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Badge, Container, Nav, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FiShoppingCart, AiOutlineUser } from '../utils/icons';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg='dark' expand='lg' variant='dark' collapseOnSelect>
@@ -16,6 +19,20 @@ const Header = () => {
               <LinkContainer to='/cart'>
                 <Nav.Link className='d-flex align-items-center gap-1'>
                   <FiShoppingCart size={20} /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge
+                      bg='success'
+                      pill
+                      style={{
+                        marginLeft: '5px',
+                      }}
+                    >
+                      {cartItems.reduce(
+                        (acc, item) => acc + item.qty,
+                        0
+                      )}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to='/login'>
